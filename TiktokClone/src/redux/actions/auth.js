2cf042f756befb7a67f6b5a2634e4428de7e3524
ChungKiet <chtkkiet@gmail.com>
2022-02-17 08:21:48 +0700

    Add Redux Function

--- TiktokClone/src/redux/actions/auth.js
+++ TiktokClone/src/redux/actions/auth.js
@@ -4,3 +4,51 @@ require('firebase/firebase-auth')
 import { USER_STATE_CHANGE} from '../constants'
 import { getPostByUser} from './post'
 
+export const userAuthStateListener = () => dispatch => {
+   firebase.auth().onAuthStateChanged((user) => {
+      if (user){
+         dispatch(getCurrentUserData())
+         dispatch(getPostByUser(firebase.auth().currentUser.uid))
+      }
+      else{
+         dispatch({ type: USER_STATE_CHANGE, currentUser: null, loaded: true })
+      }
+   })
+
+}
+
+export const getCurrentUserData = () => dispatch => {
+   firebase
+      .firestore()
+      .collection('user')
+      .doc(firebase.auth().currentUser.uid)
+      .onSnapshot((res) => {
+         if (res.exists){
+            return dispatch({
+               type: USER_STATE_CHANGE,
+               currentUser: res.data(),
+               loaded: true
+            })
+         }
+      })
+}
+
+export const login = (email, password) => dispatch => new Promise((resolve, reject) => {
+   firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+         resolve()
+      })
+      .catch((error) => {
+         reject(error)
+      })
+})
+
+export const register = (email, password) => dispatch => new Promise((resolve, reject) => {
+   firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => {
+         resolve()
+      })
+      .catch((error) => {
+         reject(error)
+      })
+})
